1116afc004e7307238a6d443e0fa7f923f1bac41
Brian Weinreich <be.weinreich@gmail.com>
2017-11-08 10:59:56 -0500

    first

---
+++ README.md
@@ -0,0 +1,9 @@
+# Robin Powered Presence Function
+
+Lambda function that receives a Density API webhook and posts to a Robin Powered space to update its presence.
+
+## Deployment
+
+`./deploy.sh` to deploy. 
+
+Note: Requires AWS CLI to deploy.
---
+++ lambda_function.py
@@ -0,0 +1,68 @@
+import os
+import json
+import requests
+
+
+# Robin Powered configuration
+robin_space_id = os.environ['ROBIN_SPACE_ID']
+robin_user_id = os.environ['ROBIN_USER_ID']  # The Density Presence Bot user_id
+robin_session_ttl = 120  # TTL in seconds
+robin_base_url = "https://api.robinpowered.com/v1.0"
+robin_space_presence_endpoint = '{}/spaces/{}/presence'.format(robin_base_url, robin_space_id)
+
+# Density API configuration
+density_space_id = os.environ['DENSITY_SPACE_ID']
+
+
+def lambda_handler(event, context):
+    webhook_space_id = event.get('space_id')
+    current_count = event.get('count')
+
+    if webhook_space_id != density_space_id:
+        return
+
+    space_is_occupied = False
+    if current_count > 0:
+        space_is_occupied = True
+
+    if space_is_occupied is True:
+        send_presence_request()
+    else:
+        clear_presence_request()
+
+    return
+
+
+def send_presence_request():
+    data = {
+      'user_id': robin_user_id,
+      'session_ttl': robin_session_ttl
+    }
+    headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Access-Token {}'.format(os.environ['API_TOKEN'])
+    }
+    requests.post(
+        robin_space_presence_endpoint,
+        headers=headers,
+        data=json.dumps(data),
+        timeout=5
+    )
+    return
+
+
+def clear_presence_request():
+    data = {
+      'user_id': robin_user_id
+    }
+    headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Access-Token {}'.format(os.environ['API_TOKEN'])
+    }
+    requests.delete(
+        robin_space_presence_endpoint,
+        headers=headers,
+        data=json.dumps(data),
+        timeout=5
+    )
+    return
